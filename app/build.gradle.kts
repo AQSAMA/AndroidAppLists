6a4aabad1bc991ae305cc50000bc604122441379
ahmed <a7m3dqa@gmail.com>
2026-01-07 15:13:57 +0300

    Fix experimental API opt-in for Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+        freeCompilerArgs += listOf(
+            "-opt-in=androidx.compose.foundation.layout.ExperimentalLayoutApi",
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+        )
     }
     buildFeatures {
         compose = true
